e06ba082d623dd2f99b993ed63aee792d28a7a45
Viveca Lindahl <vivecalindahl@gmail.com>
2018-02-08 10:40:54 +0100

    Added more ouput.

--- sequence-to-pdb/sequence-to-pdb.sh
+++ sequence-to-pdb/sequence-to-pdb.sh
@@ -9,7 +9,7 @@ x3dna_tar=./tools/x3dna-v${x3dna_version}-linux-64bit.tar.gz
 
 out_dir="./build"
 x3dna_dir="./${out_dir}/x3dna-v${x3dna_version}"
-[ ! -e "$x3dna_dir" ] && tar pzxvf "$x3dna_tar" -C "./${out_dir}"
+[ ! -e "$x3dna_dir" ] && mkdir -p $out_dir && echo "Bulding x3dna..." && tar pzxvf "$x3dna_tar" -C "./${out_dir}" &> /dev/null
 [ ! -e "$x3dna_dir" ] && echo "$x3dna_dir not found. Aborting." && exit 1
 
 # Environment variable required to set by x3dna
@@ -22,8 +22,9 @@ fiber=${x3dna_dir}/bin/fiber
 sequences="./sequences.txt"
 [ ! -e "$sequences" ] && echo "$sequences not found. Aborting." && exit 1
 
-pdb_dir="./${out_dir}/pdbs"
+pdb_dir="${out_dir}/pdbs"
 mkdir -p $pdb_dir
+echo  "Building pdb files with sequences from $sequences"
 for seq in `cat ${sequences}`; do
     $fiber -seq=${seq} -b ${pdb_dir}/${seq}.pdb || { echo "Something is probably wrong with sequence '$seq' found in $sequences" && break; };
 done
