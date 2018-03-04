c9da6e5cd4b9ea2c0c5b0a884232c1f66bf26c5f
Viveca Lindahl <vivecal@kth.se>
2018-03-04 17:44:31 +0100

    edits of launch script after debugging and testing

--- scripts/launch-mdrun/run-replicated-multiwalker-from-template.sh
+++ scripts/launch-mdrun/run-replicated-multiwalker-from-template.sh
@@ -7,6 +7,7 @@ function print_usage_exit
     exit 0
 }
 
+# Read the arguments and/or print usage
 nargsmin=3
 [ "${#args[@]}" -lt $nargsmin ] && { echo "Too few arguments given." && print_usage_exit; }
 template=${args[0]}
@@ -19,19 +20,16 @@ for ((i=2; i<${#args[@]}; i++)); do
     nwalkers+=(${args[$i]})
 done
 
-echo "Making $nreplicas replicas for each number of walkers ${nwalkers[@]} from $template"
+# Make the directory structure and generate the tprs using gmx grompp
+echo "Making $nreplicas replicas for ${nwalkers[@]} number of walkers using template: $template"
+
+# My own gmx build, has the same dependencies as the gromacs module.
+module load gromacs/2018.1
+gmx_login=/cfs/klemming/nobackup/v/vivecal/programs/gromacs-login/2018.1/bin/gmx
+gmx_mpi=/cfs/klemming/nobackup/v/vivecal/programs/gromacs/2018.1/bin/gmx
 
 start=`pwd -P`
-
-# gromacs binary
-module load gromacs/2018.1
-#gmx=gmx_seq
-
-# My own gmx build                                                                                                                                                                                                                                                                                                           
-gmx_login=/cfs/klemming/nobackup/v/vivecal/programs/gromacs-login/2018.1/bin/gmx
-
-
-# work from the template, to generate tprs
+# work in a copy of the template directory to generate tprs
 cp -r template template_work
 cd template_work
 for n in ${nwalkers[@]}; do	
@@ -56,3 +54,89 @@ for n in ${nwalkers[@]}; do
 done
 cd $start
 rm -r template_work
+
+# Launch the simulations. Split the job of each replica into many shorter jobs.
+# Each replica gets its own name, then use the sbatch flag
+# --dependency=singleton to link jobs with the same name together.
+
+# Generate a slurm job script for the given number of walkers and with
+# a unique job name given by the replica index.
+generate_job_script()
+{
+nw=$1 # number of walkers
+i=$2  # replica index (warning for bad variable name...)   
+job_out=$3 # Name of file to write to.
+
+# sbatch arguments
+time="00:15:00"
+account="2017-11-25"
+jobname="dna-x${nw}-${i}"
+ntasks=$((32*nw))
+nodes=$((1*nw))
+ntaskspernode=32
+cpuspertask=1
+constraint="[group-0|group-1|group-2|group-3|group-4]"
+
+# Settings for launching mdrun using sbatch
+
+hours=$(echo "${time}" | awk -F ':' '{print $1 + $2/60 + $3/(60*60)}')
+cont_opts="-cpi -maxh $hours"
+npme=2; nstlist=40; dlb=no; ntomp=2;
+std_opts="-pin on -quiet -v -stepout 10000 -nstlist ${nstlist} -dlb ${dlb} -npme ${npme} -ntomp ${ntomp}"
+walker_indices=($(seq  0 $((nw-1))))
+
+walker_dirs=()
+prefix='walker-'
+for j in ${walker_indices[@]}; do
+    walker_dirs+=("${prefix}${j}")
+done
+
+mdrun="$gmx_mpi mdrun $std_opts $cont_opts -multidir ${walker_dirs[@]}"
+
+aprun_opts="-cc none"
+aprun_opts+=" -n $ntasks -N $ntaskspernode"
+mdrun_cmd="aprun $aprun_opts $mdrun"
+
+echo -e \
+"#!/bin/bash -l
+
+#SBATCH --account=${account}
+#SBATCH --time=${time}
+#SBATCH --job-name=${jobname}
+#SBATCH --ntasks=${ntasks}
+#SBATCH --nodes=${nodes}
+#SBATCH --ntasks-per-node=${ntaskspernode}
+#SBATCH --cpus-per-task=${cpuspertask}
+#SBATCH --constraint=${constraint}
+
+export MPICH_GNI_MAX_EAGER_MSG_SIZE=131072 # maybe useful anymore
+
+module load gromacs/2018.1
+
+echo '---> Running:' $mdrun_cmd
+
+$mdrun_cmd
+
+echo '---> Reached end of run script.'
+" \
+> $job    
+}
+
+# Here calculate the number of jobs to run based on the maximum total allocated run time.
+totalhours=24
+# no. of jobs = total time/time per job
+njobs=$(awk -v hours=$hours -v totalhours=$totalhours 'BEGIN{print int(totalhours/hours)}')
+
+for nw in ${nwalkers[@]}; do
+    for ((i=0; i<${nreplicas}; i++)); do
+
+	mdrundir=${start}/data/${n}-walkers/replica-${i}
+	cd $mdrundir 
+	job=x${nw}-${i}.job
+	generate_job_script $nw $i $job
+
+        # launch dependent (chained) jobs for this replica
+	for ((j=0; j<${njobs}; j++)); do 
+	    sbatch --dependency=singleton $job
+	done
+done
